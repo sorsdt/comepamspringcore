3ef995c26c00a3e6b69b46cd3bad0c2365f89379
Denys_Titarenko <sorsdt@gmail.com>
2017-12-01 18:08:09 +0200

    experiments with properties

--- src/main/java/ua/epam/spring/core/App.java
+++ src/main/java/ua/epam/spring/core/App.java
@@ -7,11 +7,17 @@ import ua.epam.spring.core.beans.Event;
 import ua.epam.spring.core.loggers.EventLogger;
 
 import java.util.List;
+import java.util.TreeMap;
 
 public class App {
     private Client client;
     private EventLogger eventLogger;
     private List<String> eventMessages;
+    static private TreeMap<String, String> test;
+
+    public void setTest(TreeMap<String, String> test1) {
+        test = test1;
+    }
 
     public App(Client client, EventLogger eventLogger) {
         this.client = client;
@@ -26,6 +32,7 @@ public class App {
             event.setMsg(message);
             app.logEvent(event);
         }
+        System.out.println(test);
         ctx.close();
     }
 
